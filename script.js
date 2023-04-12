8e1b80a6ee0c4eeae5af038c85e11bddc08be368
emma jamieson <emmaj86@users.noreply.github.com>
2023-04-12 12:34:13 +0100

    updated js file

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ function getComputerChoice() {
             return 'scissors';    
         };
 }
+// var getPlayerChoice = prompt("rock, paper, scissors?");
+  
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if (playerSelection == "rock" || "paper" || "scissors" ){
+        if (playerSelection == "rock" && computerSelection == "scissors"){
+            console.log ("you win " + playerSelection + " beats " + computerSelection);
+        }
+        else if (playerSelection == "paper" && computerSelection == "rock"){
+            console.log ("you win " + playerSelection + " beats " + computerSelection);
+        }
+        else if (playerSelection == "scissors" && computerSelection == "paper"){
+            console.log ("you win "+ playerSelection + " beats " + computerSelection);
+        }
+        else if (playerSelection == computerSelection){
+            console.log ("its a draw");
+        }
+        else {
+            console.log ("computer wins " + computerSelection + " beats " + playerSelection);
+        }
+    }
+   
+  }
+   
+  const playerSelection = prompt("rock, paper, scissors?"); 
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
